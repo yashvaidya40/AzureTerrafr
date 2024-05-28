8a1136c81fe62fdcf048c0597cbdc1bf04c44960
Khandu Vaidya <yash.khandu.vaidya@accenture.com>
2024-05-28 10:04:34 +0530

    push

---
+++ AzureCloudProviders.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.74.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options chsnge
+  /*
+    Multi line comment
+  */
+  features {}
+
+
+}
